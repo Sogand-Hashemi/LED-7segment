a95fb873b94c86283493ccc6bca4d52c396992fc
Sogand Hashemi <50192153+Sogand-Hashemi@users.noreply.github.com>
2019-10-27 18:21:53 +0330

    Add files via upload

---
+++ ledTest.h
@@ -0,0 +1,27 @@
+#include <mega16.h>
+#include <delay.h>
+#define xtal 8000000
+int number=0;
+void main(void)
+{
+DDRC=0xff;
+DDRD=0xff;
+PORTC=0;
+while(1)
+{
+PORTC=number;
+PORTD=0b00010101;
+delay_ms(250);
+number++;
+if(number>6)
+{
+number=0;
+continue;
+}
+PORTC=number;
+PORTD=0b00001010;
+delay_ms(250);
+number++;
+}
+}
+
